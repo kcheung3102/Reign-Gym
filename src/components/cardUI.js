0f95a6e7b64482d62e90a1f001f374ec754548ee
kcheung3102 <kcheung3102@gmail.com>
2020-08-10 11:39:22 -0400

    text changed in cardUI

--- src/components/cardUI.js
+++ src/components/cardUI.js
@@ -17,20 +17,16 @@ const cardUI = props => {
                         <Card.Text>Free 14 day Trial with a trainer</Card.Text>
                       
                         <Card.Text>Digital Fitness Program</Card.Text>
-                        
-<<<<<<< HEAD
+
                         <Card.Text>Group Exercise Classes</Card.Text>
-                        <div className="mt-4">
-=======
-                        <Card.Text>Lorem ipsum dolor sit amet</Card.Text>
+                
                         <div className="mt-5">
->>>>>>> 6c2c57fda90a183fdc4612b009529cd8b5b61470
                             <Button variant="danger btn-radius">Sign Up</Button>{' '}
                         </div>
                     </Card.ImgOverlay>
             </Card>
         
     )
-}
+};
 
 export default cardUI;
